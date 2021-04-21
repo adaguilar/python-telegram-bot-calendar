25ff7cf512a00ea777f534c02e8700a2259f5dfa
adaguilar@frd.utn.edu.ar <adaguilar@frd.utn.edu.ar>
2021-04-21 18:37:57 -0300

    Spanish, italian and french added to MONTHS and DAYS_OF_WEEK

--- telegram_bot_calendar/static.py
+++ telegram_bot_calendar/static.py
@@ -2,10 +2,18 @@ MONTHS = {
     'en': ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     'eo': ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "aŭg", "sep", "okt", "nov", "dec"],
     'ru': ["янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"],
+    'es': ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+    'it': ["gen", "feb", "mar", "apr", "mag", "giu", "lug", "ago", "set", "ott", "nov", "dic"],
+    'fr': ["janv", "févr", "mars", "avr", "mai", "juin", "juil", "août", "sept", "oct", "nov", "déc"],
+
 }
 
 DAYS_OF_WEEK = {
     'en': ["M", "T", "W", "T", "F", "S", "S"],
     'eo': ["L", "M", "M", "Ĵ", "V", "S", "D"],
     'ru': ["П", "В", "С", "Ч", "П", "С", "В"],
+    'es': ["Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"],
+    'it': ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"],
+    'fr': ["lun", "mar", "mer", "jeu", "ven", "sam", "dim"],
+
 }
